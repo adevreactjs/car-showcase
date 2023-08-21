edb497f02855239847de15ece2c4c8c1e998430d
adevreactjs <adevreactjs@gmail.com>
2023-08-21 15:02:14 +0300

    add SearchParams

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -2,6 +2,7 @@
 import SearchManufacturer from '@/components/SearchManufacturer';
 import Image from 'next/image';
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const SearchButton = ({ othersClasses }: { othersClasses: string }) => (
   <button type='submit' className={`-ml-3 z-10 ${othersClasses} relative`}>
@@ -17,14 +18,58 @@ const SearchButton = ({ othersClasses }: { othersClasses: string }) => (
 
 const SearchBar = () => {
   const [manufacturer, setManufacturer] = useState('');
-  const handleSearch = () => {};
+  const [model, setModel] = useState('');
+  const router = useRouter();
+
+  const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (manufacturer === '' && model === '') {
+      return alert('Please fill in the search bar');
+    }
+    updateSearchParams(model.toLowerCase(), manufacturer.toLowerCase());
+  };
+
+  const updateSearchParams = (model: string, manufacturer: string) => {
+    const searchParams = new URLSearchParams(window.location.search);
+    if (model) {
+      searchParams.set('model', model);
+    } else {
+      searchParams.delete('model');
+    }
+    if (manufacturer) {
+      searchParams.set('manufacturer', manufacturer);
+    } else {
+      searchParams.delete('manufacturer');
+    }
+    const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+    router.push(newPathName);
+  };
 
   return (
     <form className='searchbar' onSubmit={handleSearch}>
       <div className='searchbar__item'>
         <SearchManufacturer manufacturer={manufacturer} setManufacturer={setManufacturer} />
-        <SearchButton othersClasses='' />
+        <SearchButton othersClasses='sm:hidden' />
       </div>
+      <div className='searchbar__item'>
+        <Image
+          src='/model-icon.png'
+          alt='model'
+          width={25}
+          height={25}
+          className='absolute w-[20px] h-[20px] ml-4'
+        />
+        <input
+          type='text'
+          name='model'
+          value={model}
+          placeholder='Model'
+          onChange={event => setModel(event.target.value)}
+          className='searchbar__input'
+        />
+        <SearchButton othersClasses='sm:hidden' />
+      </div>
+      <SearchButton othersClasses='max-sm:hidden' />
     </form>
   );
 };
